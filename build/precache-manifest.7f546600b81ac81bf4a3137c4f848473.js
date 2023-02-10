c5cc1d4f855c75fe36cfea9609eb367d558fb1a6
jeffinjx <jeffinjaison.9@gmail.com>
2023-02-10 09:23:54 +0300

    add build

---
+++ build/precache-manifest.7f546600b81ac81bf4a3137c4f848473.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f73eca692b4e713684551cd45f7123",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78dd5eab490c59c83150",
+    "url": "/static/js/2.61ab3396.chunk.js"
+  },
+  {
+    "revision": "a4cfd7baa38c4cc5a2a3",
+    "url": "/static/js/main.c2b8f074.chunk.js"
+  },
+  {
+    "revision": "082dfd1098cf2d3a1d8d",
+    "url": "/static/js/runtime-main.45761374.js"
+  }
+]);
